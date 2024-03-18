8ff9e5df739c842048479efc0779f717f0d8a2de
Ben Klinovsky <bnk989@gmail.com>
2024-03-18 14:48:14 +0200

    add query and data file

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+import fs from 'fs'
 import express from 'express'
+import { bugService } from "./services/bug.service.js"
+
+const PORT = 3030
 
 const app = express()
 app.get('/', (req, res) => res.send('Hello there'))
-app.listen(3030, () => console.log('Server ready at port 3030'))
+
+app.get('/api/bug', (req, res) => {
+    bugService.query()
+    .then(data  => res.send(data))
+})
+// app.get('/api/bug/save', (req, res) => {})
+// app.get('/api/bug/:bugId', (req, res) => {})
+// app.get('/api/bug/:bugId/remove', (req, res) => {})
+
+
+
+app.listen(PORT, () => console.log(`Server ready at port ${PORT}`))
